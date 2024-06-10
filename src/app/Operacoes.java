92dc6692e8e17b632ef2b2b71533789daa1b2d5f
felipeDevBino <felipereisbino@gmail.com>
2024-06-10 16:46:43 -0300

    0.0.4: Desenvolvendo métodos de operação

--- src/app/Operacoes.java
+++ src/app/Operacoes.java
@@ -3,11 +3,14 @@ package app;
 public class Operacoes {
 
 	private int valorAtual = 0;
+	private int valorDeMedia = 0;
+	private int valorDeResto = 0;
 	
 	public int soma(int primeiroNumero, int segundoNumero, char operador) {
 		int resultado = (primeiroNumero + segundoNumero);
 		valorAtual += resultado;
 		
+		System.out.println("\nResultado dos números somados = " + resultado);
 		return valorAtual;
 	
 	}
@@ -15,7 +18,8 @@ public class Operacoes {
 	public int subtracao(int primeiroNumero, int segundoNumero) {	
 		int resultado = (primeiroNumero - segundoNumero);
 		valorAtual -= resultado;
-		
+
+		System.out.println("\nResultado dos números subtraidos = " + resultado);
 		return valorAtual;
 	
 	}
@@ -24,6 +28,7 @@ public class Operacoes {
 		int resultado = (primeiroNumero * segundoNumero);
 		valorAtual *= resultado;
 		
+		System.out.println("\nResultado dos números multiplicados = " + resultado);
 		return valorAtual;
 	
 	}
@@ -32,16 +37,32 @@ public class Operacoes {
 		int resultado = (primeiroNumero / segundoNumero);
 		valorAtual /= resultado;
 		
+		System.out.println("\nResultado dos números divididos = " + resultado);
 		return valorAtual;
 	
 	}
 
-	public int obtemMedia(int primeiroNumero, int segundoNumero) {
-		return 0;
+	public int obtemMedia(int quantidade) {
+		int media = 0;
+		int quantiaDeNumeros = 0;
+		
+		for(int i = 0; i < quantidade; i++) {													 //Integer.parseInt(Start.scanner.nextLine());
+			System.out.println("Digite o " + (quantiaDeNumeros + 1) + " número: "); int numero = 0;
+			media += numero;
+			quantiaDeNumeros++;
+		}
+		
+		media /= quantiaDeNumeros;
+		valorDeMedia += media;
+		
+		return valorDeMedia;
 	}
 
 	public int obtemRestoDividido(int primeiroNumero, int segundoNumero) {
-		return 0;
+		int resto = (primeiroNumero % segundoNumero);
+		valorDeResto += resto;
+
+		return valorDeResto;
 	}  
 
 }
